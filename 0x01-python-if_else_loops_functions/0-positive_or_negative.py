86a1ffc254ed842f7d15673b1b56be59b9eaabc1
Aden1ke <sarahogunlalu@.com>
2023-08-09 23:04:53 +0100

    source code

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+num = random.randint(-10, 10)
+if num < 0:
+    print(f"{num} is negative")
+elif number > 0:
+    print(f"{num} is positive")
+else:
+    print(f"{num} is zero")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for hexa_num in range(99):
+    print("{} = {}".format(hexa_num, hex(hexa_num)))
