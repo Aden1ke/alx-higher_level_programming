b4357430590ce55c8fd79c5003d0e775c8848e3a
Aden1ke <sarahogunlalu@.com>
2023-08-16 17:42:28 +0100

    linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,33 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *p2 = list;;
-	listint_t *prev = list;;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	while (list && p2 && p2->next)
+	while (list && fast && fast->next)
 	{
-		list = list->next;
-		p2 = p2->next->next;
-
-		if (list == p2)
-		{
-			list = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != list && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == list)
-					break;
-
-				list = list->next;
-			}
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-		}
 	}
-
 	return (0);
+
 }
