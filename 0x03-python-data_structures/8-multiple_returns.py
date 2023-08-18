defbac20b836a9a9ea2eefc6cbaa3078a7a78ac0
Aden1ke <sarahogunlalu@.com>
2023-08-18 14:07:05 +0100

    correction of logic

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (0, 'None')
+    if len(sentence) == 0:
+        new_tu = (0, "None")
     else:
-        return (len(sentence), sentence[0])
+        new_tu = (len(sentence), sentence[0])
+    return (new_tu)
