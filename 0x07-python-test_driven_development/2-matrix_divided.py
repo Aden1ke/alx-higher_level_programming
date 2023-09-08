5cdaac7dbe7471c7911c6e908a6c6466c52be840
Aden1ke <sarahogunlalu@.com>
2023-09-08 22:36:44 +0100

    test 2nd task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function divs all element in the matrix"""
 
+
 def matrix_divided(matrix, div):
     """
     Divide all elements of a matrix by a number.
@@ -24,9 +25,11 @@ def matrix_divided(matrix, div):
     row_length = len(matrix[0])
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
             if len(row) != row_length:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix " +
+                                "must have the same size")
     new_matrix = []
     for row in matrix:
         new_row = []
