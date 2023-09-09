cef0dad47c79242008a10a1754e1654f057b4167
Aden1ke <sarahogunlalu@.com>
2023-09-09 22:28:51 +0100

    4-print_square.py test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This function prints name of a person"""
+
+
+
+def print_square(size):
+    """
+    Print the function that prints a square.
+
+    Args:
+        size: the amount of # to print
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
