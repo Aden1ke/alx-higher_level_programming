8e8ecdc5017e908c8c682400d51b50a70b110c9f
Aden1ke <sarahogunlalu@.com>
2023-09-12 00:33:11 +0100

    geometry sqaure

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
