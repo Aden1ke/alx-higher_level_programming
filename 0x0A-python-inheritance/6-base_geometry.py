3c9a1fa48cfd1adf717b7d2d1fa9da91ccef658f
Aden1ke <sarahogunlalu@.com>
2023-09-11 23:58:47 +0100

    geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function represent base geometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
+
