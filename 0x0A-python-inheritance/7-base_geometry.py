04ed942293bbf58a9186776592fc6d47d28b4d21
Aden1ke <sarahogunlalu@.com>
2023-09-12 00:08:17 +0100

    geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a function represent base geometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
