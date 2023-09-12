d675a275ba16f6fa00fc1638d49c7f75c168c9a5
Aden1ke <sarahogunlalu@.com>
2023-09-12 21:28:18 +0100

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" write a text file and count number of text"""
+
+
+def write_file(filename="", text="")::
+    """ function that write to a text file """
+    with open(filename, 'w', encoding='utf-8') as file:
+        count = file.write(text)
+    return count
