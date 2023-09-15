6eca81a88de7b879c9793880e31d623197b2af7a
Aden1ke <sarahogunlalu@.com>
2023-09-15 22:42:34 +0100

    test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Base:
+    """
+    This class will be the “base” of all other classes in this project
+    __nb_objects is used to keep track of the num of instances created from this class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialize a new Base.
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
