932f6d46b936da8626eb98f78c0d3add779f0896
Aden1ke <sarahogunlalu@.com>
2023-11-26 10:19:39 +0100

    push change

--- 0x0D-SQL_introduction/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const argumentCount = process.argv.length;
-if (argumentCount == 2) {
-	console.log('No argument');
-}
-else {
-	console.log(process.argv[2]);
-}
---
+++ porm/0-select_states.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from MySQLdb import MySQLdb
+class states:
+    my __init_(self,mysql username, mysql password,database name):
+        self.username = mysql username
+        self.password = mysql password
+        self.dababase = database name
+    (f"id,states"zipode = 3306)
